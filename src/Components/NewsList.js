9874ca102581d78da7534134082a3bdf8e803640
yoselorm <yoselorm@gmail.com>
2022-10-12 05:14:13 -0800

    My news

--- src/Components/NewsList.js
+++ src/Components/NewsList.js
@@ -3,9 +3,9 @@ import React from 'react';
 const NewsList = (props) => {
     console.log(props)
     return (
-        <div className='p-6 flex flex-row'>
-            <div className='sm:max-h-fit sm:w-[32vw] w-[50vw]'>
-                <img src={props.image} className='sm:h-[30vh] sm:w-[28vw]' />
+        <div className='p-6 ml-4'>
+            <div className='sm:max-h-fit sm:w-[32vw] w-[85vw]'>
+                <img src={props.image} className='sm:h-[30vh] h-[30vh] w-[80vw] sm:w-[28vw]' />
                 <a href={props.url} className='text-[#3F0071] text-md font-medium hover:text-[#D800A6]'>{props.title}</a>
                 <p className='text-sm mt-2'>{props.description}</p>
             </div>
